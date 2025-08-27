ec927acc4ccfdb74a46ca000022f16f62e4452ad
pk90264180 <pk90264180@gmail.com>
2025-08-27 17:51:34 +0530

    fixed CORS

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:4000",
-      "https://ps-public-school.vercel.app/",
+      "https://ps-public-school.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
